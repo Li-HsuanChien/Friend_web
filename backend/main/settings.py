e048fe57f63c9426ea40306a9dac28702736edff
Li-HsuanChien <shanechien2023@gmail.com>
2024-03-21 12:45:27 -0400

    change django main folder name from backend to main

--- backend/main/settings.py
+++ backend/main/settings.py
@@ -91,7 +91,7 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
-ROOT_URLCONF = 'backend.urls'
+ROOT_URLCONF = 'main.urls'
 
 TEMPLATES = [
     {
